78c648f26731dba2c97ddb8959b4570afe06bc47
Dennis Mwea <mweadennis2@gmail.com>
2020-04-05 12:05:17 +0300

    Completed Conversation Screen UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Check if hello world shows up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Messio());
 
     expect(find.text("Hello Dennis!"), findsOneWidget);
   });
