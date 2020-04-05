138a2a2c89ad84992d7ddffbf6d29bdf34c4b44d
Dennis Mwea <mweadennis2@gmail.com>
2020-04-05 14:04:10 +0300

    Fix failing tests

--- test/pages/ConversationPageSlide_test.dart
+++ test/pages/ConversationPageSlide_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/pages/ConversationPage.dart';
 import 'package:messio/pages/ConversationPageSlide.dart';
+import 'package:messio/widgets/InputWidget.dart';
 
 void main() {
   const MaterialApp app = MaterialApp(
@@ -15,5 +16,6 @@ void main() {
 
     expect(find.byType(ConversationPage), findsOneWidget);
     expect(find.byType(PageView), findsOneWidget);
+    expect(find.byType(InputWidget),findsOneWidget);
   });
 }
